a1f242642461a80ab9659bf533f418ef4b0cc4fe
TulioNPL <tulionp.lopes@gmail.com>
2020-03-28 16:43:20 -0300

    Update aula - Fetch

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', function(event){
-    console.log('Instalado',event);
+self.addEventListener('install', function(e) {
+    console.log('SW: Instalado',e);
 })
 
-self.addEventListener('activate', function(event){
-    console.log('IAtivado',event);
+self.addEventListener('activate', function(e) {
+    console.log('SW: Ativado',e);
 })
+
+self.addEventListener('fetch', function(e) {
+    if(e.request.url.endsWith('/conteudo.html')) {
+        console.log('SW: Carregando', e.request.url);
+
+        e.respondWith(
+            fetch(e.request)
+                .then(function(resp){
+                    if(resp.ok) return resp;
+                    return new Response("A imagem não está disponível neste momento!")
+                })
+        )
+                
+    };
+});
