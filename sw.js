c10aba6b33ff1c1d582a05241b79ede8dc4b89a4
TulioNPL <tulionp.lopes@gmail.com>
2020-03-28 22:40:27 -0300

    Aula cache

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
 self.addEventListener('install', function(e) {
     console.log('SW: Instalado',e);
+    caches.open('PWA-v0.1').then(function(cache) {
+        cache.keys().then( function(lista) {
+            console.log(lista)
+        });
+    });
 })
 
 self.addEventListener('activate', function(e) {
     console.log('SW: Ativado',e);
-})
+});
 
 self.addEventListener('fetch', function(e) {
-    if(e.request.url.endsWith('/conteudo.html')) {
-        console.log('SW: Carregando', e.request.url);
 
-        e.respondWith(
-            fetch(e.request)
-                .then(function(resp){
-                    if(resp.ok) return resp;
-                    return new Response("A imagem não está disponível neste momento!")
-                })
-        )
-                
-    };
+    let resposta = caches.open('PWA-v0.1').then(function(cache) {
+        return cache.match(e.request).then( function(recurso){
+
+            if(recurso){
+                console.log(`Servindo ${e.request.url} do cache.`)
+                return recurso
+            }
+            console.log(`Servindo ${e.request.url} da web.`)
+            return fetch(e.request).then( function(recurso){
+                cache.put(e.request, recurso.clone());
+                return recurso;
+            })
+        })
+    })
+    e.respondWith(resposta)
 });
