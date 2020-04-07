a3d3bc015589bc3f8d39bf445dbc0c93f6f57756
TulioNPL <tulionp.lopes@gmail.com>
2020-04-07 18:49:31 -0300

    Add metodo criador de eventos

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+const events = document.querySelector('.events');
+
 document.addEventListener('DOMContentLoaded', function() {
     //menu
     const menus = document.querySelectorAll('.side-menu');
@@ -7,3 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const forms = document.querySelectorAll('.side-form');
     M.Sidenav.init(forms, {edge: 'left'});
 });
+
+//Criar evento
+const renderEvent = (data, id) => {
+
+    const html = `
+        <div class="card-panel event white row" data-id="${id}">
+            <img src="/img/Webp.net-resizeimage.png" alt="event thumb">
+            <div class="event-details">
+                <div class="event-title">${data.title}</div>
+                <div class="event-date">${data.date}</div>
+            </div>
+            <div class="event-delete">
+                <i class="material-icons" data-id="${id}>delete_outline</i>
+            </div>
+        </div>         
+    `;
+
+    events.innerHTML += html;
+
+}
